a30cca151488d71f0cc4dfb474d06741422ce22b
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-04-29 18:49:43 -0400

    Added my action.

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "./App.css";
 import Dogs from "./components/Dogs";
 
--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,9 +1,22 @@
 import axios from "axios";
 
+export const FETCHING_DOGS_START = "FETCHING_DOGS_START";
+export const FETCHING_DOGS_SUCCESS = "FETCHING_DOGS_SUCCESS";
+export const FETCHING_DOGS_FAILED = "FETCHING_DOGS_FAILED";
+
 // Fill in reducer types later
 
-export const getDogs = () => {
-  axios.get("https://dog.ceo/api/breeds/image/random/3").then((res) => {
-    console.log(res);
-  });
+export const getDogs = () => (dispatch) => {
+  dispatch({ type: FETCHING_DOGS_START });
+
+  axios
+    .get("https://dog.ceo/api/breeds/image/random/3")
+    .then((res) => {
+      console.log(res);
+      dispatch({ type: FETCHING_DOGS_SUCCESS, payload: res.message });
+    })
+    .catch((err) => {
+      console.log(err);
+      dispatch({ type: FETCHING_DOGS_FAILED, payload: "Failed" });
+    });
 };
